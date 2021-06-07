013ac5c58e42305fb40df16570f1749eb43301ef
jerrysaffold <jerrysaffold@knights.ucf.edu>
2021-06-06 22:04:14 -0400

    Final Commit

--- src/main/java/base/ex08/App.java
+++ src/main/java/base/ex08/App.java
@@ -1,4 +1,53 @@
 package base.ex08;
+/*
+ *  UCF COP3330 Summer 2021 Assignment 8 Solution
+ *  Copyright 2021 Jerry Saffold
+ */
 
+import java.util.Scanner;
+
+/*
+Exercise 8 - Pizza Party
+Division isn’t always exact, and sometimes you’ll write programs that will need to deal with the leftovers as a whole number
+instead of a decimal.
+
+Write a program to evenly divide pizzas.
+Prompt for the number of people, the number of pizzas, and the number of slices per pizza.
+Ensure that the number of pieces comes out even. Display the number of pieces of pizza each person should get.
+If there are leftovers, show the number of leftover pieces.
+
+Example Output
+How many people? 8
+How many pizzas do you have? 2
+How many slices per pizza? 8
+8 people with 2 pizzas (16 slices)
+Each person gets 2 pieces of pizza.
+There are 0 leftover pieces.
+
+Challenges
+Revise the program to ensure that inputs are entered as numeric values.
+Don’t allow the user to proceed if the value entered is not numeric.
+Alter the output so it handles pluralization properly, for example: "Each person gets 2 pieces of pizza."
+or "Each person gets 1 piece of pizza." Handle the output for leftover pieces appropriately as well.
+Create a variant of the program that prompts for the number of people and the number of pieces each person wants,
+and calculate how many full pizzas you need to purchase.
+ */
 public class App {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("How many people? ");
+        int people = input.nextInt();
+        System.out.print("How many pizzas do you have? ");
+        int num_pizzas = input.nextInt();
+        System.out.print("How many slices per pizza? ");
+        int slices_per_pizza = input.nextInt();
+
+        int num_slices = num_pizzas * slices_per_pizza;
+        int slices_per_person = num_slices / people;
+        int leftover = num_slices % people;
+
+        System.out.printf("%d people with %d pizzas (%d slices)\n", people, num_pizzas, num_slices);
+        System.out.printf("Each person gets %d pieces of pizza.\nThere are %d leftover pieces",
+                slices_per_person,leftover);
+    }
 }
